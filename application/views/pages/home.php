f3c4dedf5ccf245162bafb99bd857d91d572a538
pedrogrempereira <pedropereiramsft@outlook.com>
2017-02-16 00:36:57 +0000

    updated model

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -13,22 +13,22 @@
             <img data-src="<?php echo base_url("assets/images/sb_1.jpg");?>" alt="900x500" src="<?php echo base_url("assets/images/banner-1.jpg");?>">
             <div class="carousel-caption">
                 <h3> Varial Kick Flip </h3>
-                <p> Do a Kick Flip or some serious Technical Skating on Motion Board 9000</p>
+                <p> Do a Kick Flip or some serious Technical Skating on Motion Board 8989</p>
             </div>
         </div>
         <div class="item" onclick="location.href='<?php echo base_url();?>product'">
             <img data-src="<?php echo base_url("assets/images/sb_2.jpg");?>" alt="900x500" src="<?php echo base_url("assets/images/banner-2.jpg");?>">
             <div class="carousel-caption">
                 <h3> Carve </h3>
-                <p> Carve your new style on Motion 9000.</p>
+                <p> Carve your new style on Motion 19999.</p>
             </div>
         </div>
 
         <div class="item active" onclick="location.href='<?php echo base_url();?>product'">
             <img data-src="<?php echo base_url("assets/images/sb_4.jpg");?>" alt="900x500" src="<?php echo base_url("assets/images/banner-3.jpg");?>">
             <div class="carousel-caption">
-                <h3>Pro Skate Board Motion 9000</h3>
-                <p> Do all your cool stuff on Contoso board Motion 9000.</p>
+                <h3>Pro Skate Board Motion 19999</h3>
+                <p> Do all your cool stuff on Contoso board Motion 1999.</p>
             </div>
         </div>
     </div>
